d6b411630187eaeb0b9887d2a599da878e550957
code-knayam <mayankjain1394@gmail.com>
2020-05-10 16:38:19 +0530

    Updated user search api to allow regex

--- src/main/java/com/knayam/instagramapi/queries/UserDetailsQueries.java
+++ src/main/java/com/knayam/instagramapi/queries/UserDetailsQueries.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.mongodb.core.query.TextQuery;
@@ -21,11 +22,10 @@ public class UserDetailsQueries {
 	}
 	
 	public List<UserDetails> findByFreeText(String text) {
-		TextCriteria criteria = TextCriteria.forDefaultLanguage().matching(text);
-		
-		Query query = TextQuery.queryText(criteria)
-				.sortByScore()
-				.with(PageRequest.of(0, 3));
+		Query query = Query.query(
+				new Criteria().orOperator(
+						Criteria.where("username").regex(text, "im"),
+						Criteria.where("name").regex(text, "im")));
 		
 		return this.mongoTemplate.find(query, UserDetails.class);
 	}
